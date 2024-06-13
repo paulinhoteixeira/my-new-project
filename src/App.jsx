347d4fb16238c3dd917336b22d184f5bb9c8dcb5
Paulo Teixeira De Oliveira <paulo.teixeira@linx.com.br>
2024-06-13 00:23:02 -0300

    add Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+import './styles/App.css'
+
+class App extends React.Component {
+  
+  render(){
+    return (
+      <Navbar/>
+      
+    );
+  }
+}
+
+export default App;
