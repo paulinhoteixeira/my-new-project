002e42fce0d148d8ad5a60ef92d7d8a2788c14dd
Paulo Teixeira De Oliveira <paulo.teixeira@linx.com.br>
2024-06-13 18:21:17 -0300

    add counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { Article } from "./components/Article/Article";
+import article1Img from "./assets/images/article1.png"
+import article2Img from "./assets/images/article2.png"
+import article3Img from "./assets/images/article3.png"
 
 import './styles/App.css'
 
@@ -7,7 +11,18 @@ class App extends React.Component {
   
   render(){
     return (
+      <>
       <Navbar/>
+      <section id="articles">
+        <Article title="Designing Dashboards" provider="NASA" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img}/>
+        <Article title="Vibrant Portraits of 2020" provider="SpaceNews" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article2Img}/>
+        <Article title="36 Days of Malayalam type" provider="Spaceflight Now" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article3Img}/>
+        <Article title="Designing Dashboards" provider="NASA" description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet." thumbnail={article1Img}/>
+        
+
+      </section>
+      
+      </>
       
     );
   }
